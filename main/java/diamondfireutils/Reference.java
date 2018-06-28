8a8f145a03125d3441aa609ffbf443e87b2307c1
KSashaDF <40648165+KSashaDF@users.noreply.github.com>
2018-06-28 13:00:04 -0700

    Delete Reference.java

--- main/java/diamondfireutils/Reference.java
+++
@@ -1,9 +0,0 @@
-package diamondfireutils;
-
-public class Reference {
-
-    static final String MOD_ID = "dfutils";
-    static final String NAME = "DiamondFire Utilities";
-    static final String VERSION = "1.0";
-    static final String ACCEPTED_VERSIONS = "[1.12]";
-}
