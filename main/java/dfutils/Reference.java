f26f830885f4ca2f0324c1912184b5e029fafdd9
KSashaDF <40648165+KSashaDF@users.noreply.github.com>
2018-07-14 15:46:04 -0700

    v1.1.2
    + Added /code import <file name>
    + Added /code export <file name>

--- main/java/dfutils/Reference.java
+++ main/java/dfutils/Reference.java
@@ -4,6 +4,6 @@ class Reference {
 
     static final String MOD_ID = "dfutils";
     static final String NAME = "DiamondFire Utilities";
-    static final String VERSION = "1.1.1";
+    static final String VERSION = "1.1.2";
     static final String ACCEPTED_VERSIONS = "[1.12]";
 }
--- main/java/dfutils/codetools/CodeData.java
+++ main/java/dfutils/codetools/CodeData.java
@@ -4,11 +4,11 @@ import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.nbt.NBTException;
 import net.minecraft.nbt.NBTTagCompound;
 
+import org.apache.commons.compress.utils.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class CodeData {
     
@@ -17,8 +17,9 @@ public class CodeData {
     public CodeData() {
 
         try {
+
             //Reads codeData.json and converts it into an NBT tag.
-            codeReferenceData = JsonToNBT.getTagFromJson(IOUtils.toString(new InputStreamReader(this.getClass().getResourceAsStream("/assets/dfutils/codeData.json"))));
+            codeReferenceData = JsonToNBT.getTagFromJson(IOUtils.toString(this.getClass().getResourceAsStream("/assets/dfutils/codeData.json"), Charsets.UTF_8));
         } catch (IOException exception) {
             LogManager.getLogger().error("Uh oh! Encountered IO Exception while loading code reference data...");
         } catch (NBTException exception) {
