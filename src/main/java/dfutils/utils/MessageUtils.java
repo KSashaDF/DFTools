5e6237b90d0cf67cab54f3614ce248f205f5a11d
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-07-21 19:00:18 +0200

    Changed and added 1 prefix

--- src/main/java/dfutils/utils/MessageUtils.java
+++ src/main/java/dfutils/utils/MessageUtils.java
@@ -10,6 +10,7 @@ public class MessageUtils {
     private static final String ERROR_PREFIX = "§4\u258E §c§lERROR §4> §c";
     private static final String INFO_PREFIX = "§1\u258E §9§lINFO §1> §9";
     private static final String ACTION_PREFIX = "§b\u258E §d§lACTION §b> §d";
+    private static final String NOTE_PREFIX = "§6\u258E§4§k::§c§l NOTE §4§k::§6> §c§o";
     
     public static void errorMessage(String message) {
         minecraft.player.sendMessage(new TextComponentString(ERROR_PREFIX + message));
@@ -21,5 +22,9 @@ public class MessageUtils {
     
     public static void actionMessage(String message) {
         minecraft.player.sendMessage(new TextComponentString(ACTION_PREFIX + message));
+    }
+
+    public static void noteMessage(String message) {
+        minecraft.player.sendMessage(new TextComponentString(NOTE_PREFIX + message));
     }
 }
