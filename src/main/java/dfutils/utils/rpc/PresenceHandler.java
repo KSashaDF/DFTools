7c13dd82edfce3b8ee44f1f8a033f9b5c32086ff
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-08-07 21:04:05 +0200

    Discord RPC - Stage 1

---
+++ src/main/java/dfutils/utils/rpc/PresenceHandler.java
@@ -0,0 +1,35 @@
+package dfutils.utils.rpc;
+
+// -------------------------
+// Created by: Timeraa
+// Created at: 07.08.18
+// -------------------------
+
+
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
+
+public class PresenceHandler {
+    public static void updatePresence() {
+        DiscordRPC lib = DiscordRPC.INSTANCE;
+        String applicationId = "476455349780611072";
+        String steamId = "";
+        DiscordEventHandlers handlers = new DiscordEventHandlers();
+        handlers.ready = (user) -> System.out.println("Ready!");
+        lib.Discord_Initialize(applicationId, handlers, true, steamId);
+        DiscordRichPresence presence = new DiscordRichPresence();
+        presence.startTimestamp = System.currentTimeMillis() / 1000; // epoch second
+        presence.details = "Testing RPC";
+        lib.Discord_UpdatePresence(presence);
+        // in a worker thread
+        new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                lib.Discord_RunCallbacks();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ignored) {}
+            }
+        }, "RPC-Callback-Handler").start();
+    }
+}
