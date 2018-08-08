0056e62110e260362333777c64100067953ad15d
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-08-08 05:08:11 +0200

    Discord RPC - Stage 3 - Configuration File

--- src/main/java/dfutils/config/ConfigHandler.java
+++ src/main/java/dfutils/config/ConfigHandler.java
@@ -1,6 +1,7 @@
 package dfutils.config;
 
 import dfutils.Reference;
+import dfutils.utils.rpc.PresenceHandler;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,9 +14,10 @@ public class ConfigHandler {
 
     static Configuration config;
 
-    public static boolean DO_QUICK_ITEM_RENAME = false;
+    public static boolean DO_QUICK_ITEM_RENAME = true;
     public static boolean DO_VARPURGE_CONFIRM = false;
     public static boolean DO_PLOTCLEAR_CONFIRM = true;
+    public static boolean DISCORD_RPC_ENABLED = true;
 
     public static void init(File configFile) {
 
@@ -25,6 +27,9 @@ public class ConfigHandler {
         //Creates configuration categories.
         config.addCustomCategoryComment("Settings", "Enable or disable various features here.");
 
+        //Creates configuration categories.
+        config.addCustomCategoryComment("Discord RPC", "Disable or enable certain features from your Discord RPC");
+
         reloadConfig();
     }
 
@@ -32,6 +37,7 @@ public class ConfigHandler {
         DO_QUICK_ITEM_RENAME = config.getBoolean("Quick Item Rename", "Settings", false, "Enable this to make it so you can shift + left click any item to quickly rename it.");
         DO_VARPURGE_CONFIRM = config.getBoolean("/plot varpurge confirm", "Settings", false, "Enable this to make it so you need to type an extra confirmation command to varpurge.");
         DO_PLOTCLEAR_CONFIRM = config.getBoolean("/plot clear confirm", "Settings", true, "Disable this to make it so you do not need to type an extra confirmation command to clear a plot.");
+        DISCORD_RPC_ENABLED = config.getBoolean("Enabled", "Discord RPC", true, "Enable or disable Discord RPC.");
 
         if (config.hasChanged()) {
             config.save();
@@ -43,6 +49,8 @@ public class ConfigHandler {
 
         if (event.getModID().equalsIgnoreCase(Reference.MOD_ID)) {
             reloadConfig();
+
+            if(DISCORD_RPC_ENABLED) PresenceHandler.updatePresence(); else PresenceHandler.destroyPresence();
         }
     }
 }
